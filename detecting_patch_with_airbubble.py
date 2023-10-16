cf4891270573216c9a433bbfb051e25e2e3a06bd
mrahmansagar <mrahman.sagar@gmail.com>
2023-10-16 17:03:26 +0200

    updated to deal with any size of patches

--- detecting_patch_with_airbubble.py
+++ detecting_patch_with_airbubble.py
@@ -20,8 +20,8 @@ from proUtils import utils
 
 import methods
 
-data_dir = 'F:\\MD_1264_A6_1_Z3.3mm_corr_phrt\\slices'
-slice_no = 'slice_0850.tif'
+data_dir = 'F:\\MD_1264_A3_1_Z9.9mm\\slices'
+slice_no = 'slice_0003.tif'
 
 new_air = 0.00009706
 new_paraffin = 0.0004179
@@ -44,7 +44,12 @@ imarray = utils.norm8bit(imarray)
 rotated_image, _, lowdist = utils.tilt_correction(imarray, edge_th=0.1, ang_vari=4, plot=True)
 _, _, highdist = utils.tilt_correction(imarray, edge_th=250)
 
-croped_imarray = rotated_image[int(abs(lowdist[0])+100):int(abs(highdist[0])), :]
+start_point = int(abs(lowdist[0]))
+end_point = int(abs(highdist[0]))
+h_offset = 150
+
+croped_imarray = rotated_image[start_point+h_offset:end_point, :]
+
 # croped_imarray = croped_imarray[:, 500:3300]
 plt.figure(figsize=(12,6))
 plt.imshow(croped_imarray, cmap='gray')
@@ -60,13 +65,14 @@ air_imarray = nd.binary_opening(air_imarray, np.ones((10, 10)))
 plt.figure(figsize=(12,6))
 plt.imshow(air_imarray, cmap='gray')
 
+patch_size = 550
 
 object_params, labels = methods.calc_binObject_params(air_imarray, maxObj_height=512, maxObj_width=512, plot=True)
 
-patch_results = methods.detect_patch_with_object(croped_imarray, labels)
+patch_results = methods.detect_patch_with_object(croped_imarray, labels, patch_size=patch_size, step_size=patch_size)
 
 
-croped_imarray, patch_results = methods.validate_patches(rotated_image, lowdist, 100, highdist, patch_results)
+croped_imarray, patch_results = methods.validate_patches(rotated_image, croped_imarray, start_point, h_offset, end_point, patch_results, patch_size=patch_size)
 
 # loading the model which is trained for 30 epochs
 from keras.models import load_model
@@ -74,21 +80,30 @@ model = load_model('models/src2tar_with_real_air_after_165000.h5')
 
 removed_air = np.copy(croped_imarray)
 
+input_patch_size = (256, 256)
+target_patch_size = (patch_size, patch_size)
+
+def resize_image(image, target_shape):
+    return nd.zoom(image, (target_shape[0] / image.shape[0], target_shape[1] / image.shape[1]))
+
+
+
 for params in patch_results:
     
     if params['num_objects_in_patch'] > 0:
         x1, y1 = params['patch_coords'][0]
         x2, y2 = params['patch_coords'][1]
 
-        
         patch = croped_imarray[y1:y2, x1:x2]
-#         patch = nd.zoom(patch, zoom=0.5)
+        patch = resize_image(patch, input_patch_size)
+        # patch = nd.zoom(patch, zoom=0.5)
         patch_inShape = np.expand_dims((patch-127.5)/127.5, axis=(0, -1))
         air_rem_patch = np.squeeze(model.predict(patch_inShape, verbose=0))
         air_rem_patch = (air_rem_patch+1)/2.0
         air_rem_patch = img_as_ubyte(air_rem_patch)
+        air_rem_patch = resize_image(air_rem_patch, target_patch_size)
         removed_air[y1:y2, x1:x2] = air_rem_patch
-#         removed_air[y1:y2, x1:x2] = nd.zoom(air_rem_patch, zoom=2)
+        # removed_air[y1:y2, x1:x2] = nd.zoom(air_rem_patch, zoom=2)
 
 
 plt.figure(figsize=(16,8))       
